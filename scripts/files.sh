aeba268c5b886c71896dca3edcdfa54cf108c0bd
dave <dmh2000@gmail.com>
2025-01-20 08:15:05 -0800

    files.sh

--- scripts/files.sh
+++ scripts/files.sh
@@ -5,34 +5,34 @@
 extensions=(
     ".go"			#:Go
     ".py"			#:Python
-    ".js"			#:JavaScript
-    ".java"			#:Java
-    ".cpp"			#:C++
-    ",cxx"			#:C++
-    ".cc"			#:C++
-    ".c"			#:C
-    ".cs"			#:C			#
-    ".php"			#:PHP
-    ".rb"			#:Ruby
-    ".swift"		#:Swift
-    ".ts"			#:TypeScript
-    ".html"			#:HTML
-    ".htm"			#:HTML
-    ".css"			#:CSS
-    ".sql"			#:SQL
-    ".pl"			#:Perl
-    ".sh"			#:Shellscript
-    ".r"			#:R
-    ".m"			#:MATLABorObjective-C
-    ".scala"		#:Scala
-    ".kt"			#:Kotlin
-    ".rs"			#:Rust
-    ".hs"			#:Haskell
-    ".lua"			#:Lua
-    ".vb"			#:VisualBasic
-    ".f"			#:Fortran
-    ".f90"			#:Fortran
-    ".jl"           #:Julia
+    # ".js"			#:JavaScript
+    # ".java"		#:Java
+    # ".cpp"		#:C++
+    # ",cxx"		#:C++
+    # ".cc"			#:C++
+    # ".c"			#:C
+    # ".cs"			#:C			
+    # ".php"		#:PHP
+    # ".rb"			#:Ruby
+    # ".swift"		#:Swift
+    # ".ts"			#:TypeScript
+    #".html"		#:HTML
+    #".htm"			#:HTML
+    #.css"			#:CSS
+    # ".sql"		#:SQL
+    # ".pl"			#:Perl
+    # ".sh"			#:Shellscript
+    # ".r"			#:R
+    # ".m"			#:MATLABorObjective-C
+    # ".scala"		#:Scala
+    # ".kt"			#:Kotlin
+    # ".rs"			#:Rust
+    # ".hs"			#:Haskell
+    # ".lua"		#:Lua
+    # ".vb"			#:VisualBasic
+    # ".f"			#:Fortran
+    # ".f90"		#:Fortran
+    # ".jl"          #:Julia
 )
 current_dir=$(pwd)
 find . -type f | while read -r file; do
