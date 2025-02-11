3810c18e72b2c23322e5d0a96038fc12633a6d25
Dave <dmh2000@gmail.com>
2025-02-11 10:23:36 -0800

    chore: Remove unnecessary commented-out text in main.go

--- cmd/sqirvy/main.go
+++ cmd/sqirvy/main.go
@@ -112,5 +112,3 @@ func main() {
 
 	os.Exit(0)
 }
-
-// "```https://sqirvy.xyz\n   Skip to content    Sqirvy.xyz              \nPosts\n   \nTags\n   \nAbout\n                                 Sqirvy.xyz Sqirvy.xyz is Dave's blog that no one looks at (:)       Featured  Adding AI To Golang AppsPosted on:January 27, 2025 | \u00a0at\u00a004:00 PMThree approaches to using AI models in GolangClone A Web Page Layout From A ScreenshotPosted on:September 3, 2024 | \u00a0at\u00a005:00 PMHow to clone a website layout from a screenshotTypes of Embedded DevelopersPosted on:April 28, 2024 | \u00a0at\u00a005:00 PMT...+712 more"
